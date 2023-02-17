bda874955094e710f7c756be9843af2194bb2019
insider-avinash <gavandheavinash12@gmail.com>
2023-02-17 20:30:56 +0530

    upload in node js

---
+++ Day11/upload/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const multer = require("multer");
+const app = express();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "upload");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + "-" + Date.now() + ".jpeg");
+    },
+  }),
+}).single("user_file");
+
+app.post("/upload", upload, (req, res) => {
+  res.send("file upload");
+});
+
+app.listen(3000);
